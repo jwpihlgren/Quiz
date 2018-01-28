abc93e78d888fccfa638acc3962a6d634a8b30f4
Aeon <j.pihlgren@outlook.com>
2018-01-28 04:52:35 +0100

    Started building on the View and created a Demo class

---
+++ src/Demo.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * @author Joachim Pihlgren, joapih-6
+ */
+public class Demo
+{
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(() -> new Controller());
+	}
+}
