aa12ca06fd0b9c6ae64a2df2c5ea31fe43d42692
JPihl <j.pihlgren@outlook.com>
2018-01-31 12:54:43 +0100

    -Added a new view for questions

---
+++ src/QuestionView.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * @AUTHOR Joachim Pihlgren, joapih-6
+ */
+public class QuestionView extends JFrame implements Observer
+{
+
+	public QuestionView()
+	{
+
+	}
+
+	@Override public void update(Observable o, Object arg)
+	{
+
+	}
+}
